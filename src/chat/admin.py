32ff500626ddc33adeff80b13f79c3a9c666a655
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-10-22 04:15:13 -0500

    Django Channels 05 -> Done
    Chat models and API

--- src/chat/admin.py
+++ src/chat/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Message
+from .models import Message, Contact, Chat
 # Register your models here.
 
+admin.site.register(Contact)
+admin.site.register(Chat)
 admin.site.register(Message)
